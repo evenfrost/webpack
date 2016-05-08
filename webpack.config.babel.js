b2d489effc04ffea8af33206a0855ade20bc77bf
evenfrost <kisolov@gmail.com>
2016-05-08 17:21:16 +0300

    Add webpack dev config

--- webpack.config.babel.js
+++
@@ -1,9 +0,0 @@
-import { resolve } from 'path';
-
-export default {
-  entry: './index',
-  output: {
-    path: resolve(__dirname),
-    filename: 'bundle.js',
-  },
-};
---
+++ webpack.config.dev.babel.js
@@ -0,0 +1,36 @@
+import { resolve } from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:5000',
+    'webpack/hot/dev-server',
+    './client/index',
+  ],
+  output: {
+    path: resolve(__dirname, 'client'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    loaders: [{
+      test: /\.js/,
+      exclude: /node_modules/,
+      loader: 'babel',
+    }, {
+      test: /\.styl$/,
+      exclude: /node_modules/,
+      loader: 'style!css!stylus',
+    }],
+  },
+  devServer: {
+    contentBase: resolve(__dirname, 'client'),
+    hot: true,
+    port: 5000,
+  },
+};
